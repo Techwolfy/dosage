ae5417f8d2e630c409059e44b25fa2ee079fd3af
Techwolf <dring@wolfishly.me>
2019-07-28 18:32:22 -0700

    Add 21stCenturyFox (ignores robots.txt)

--- dosagelib/plugins/num.py
+++ dosagelib/plugins/num.py
@@ -5,9 +5,21 @@
 
 from __future__ import absolute_import, division, print_function
 
+from ..scraper import _ParserScraper
 from .common import _WordPressScraper
 
 
 class NineteenSeventySeven(_WordPressScraper):
     name = '1977'
     url = 'http://1977thecomic.com/'
+
+
+class TwentyFirstCenturyFox(_ParserScraper):
+	name = '21stCenturyFox'
+	url = 'http://www.hirezfox.com/21cf/'
+	stripUrl = url + 'd/%s.html'
+	firstStripUrl = stripUrl % '20010820'
+	imageSearch = '//img[contains(@src, "21cf/comics/")]'
+	prevSearch = '//a[./img[contains(@src, "previous_day")]]'
+	multipleImagesPerStrip = True
+	ignoreRobotsTxt = True
