946cf9bde0d3a766d7ba684cac5deb6583aadb84
Techwolf <dring@wolfishly.me>
2019-08-16 19:11:14 -0700

    Add ComicFury/ResNulliusCS

--- dosagelib/plugins/comicfury.py
+++ dosagelib/plugins/comicfury.py
@@ -841,6 +841,7 @@ class ComicFury(_ParserScraper):
             cls('ReSetArt', 'resetfanarts'),
             cls('ResidentWeirdo', 'residentweirdo'),
             cls('ResNullius', 'resnullius'),
+            cls('ResNulliusCS', 'resnulliuscs'),
             cls('ReturnOfWonderland', 'returnofwonderland'),
             cls('Revive', 'revive'),
             cls('RexAfterDark', 'rexafterdark'),
