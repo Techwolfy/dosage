552b304f29b6a09cb0be182aeca7f085546634fc
Techwolf <dring@wolfishly.me>
2022-02-19 01:51:15 -0800

    Fix Guardia

--- dosagelib/plugins/old.py
+++ dosagelib/plugins/old.py
@@ -1448,6 +1448,7 @@ class Renamed(Scraper):
             cls('GoComics/PeanutsEnEspanol', 'GoComics/SnoopyEnEspanol'),
             cls('GoComics/RipleysBelieveItOrNotSpanish', 'GoComics/RipleysAunqueUstedNoLoCrea'),
             cls('GoComics/WebcomicName', 'WebcomicName'),
+            cls('Guardia', 'ComicFury/Guardia'),
             cls('Shivae/BlackRose', 'BlackRose'),
             cls('SmackJeeves/BlackTapestries', 'ComicFury/BlackTapestries'),
             cls('SmackJeeves/ByTheBook', 'ByTheBook'),
