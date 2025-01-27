84b66dbf7be787024ea4c86e310d669f1f7e0152
Techwolf <dring@wolfishly.me>
2025-01-26 22:56:15 -0800

    Fix Derideal

--- dosagelib/plugins/derideal.py
+++ dosagelib/plugins/derideal.py
@@ -6,13 +6,13 @@ from ..helpers import indirectStarter
 
 
 class Derideal(_ParserScraper):
-    baseUrl = 'https://www.derideal.com/'
-    imageSearch = '//img[contains(@class, "comic-page")]'
-    prevSearch = '//a[i[contains(@class, "fa-angle-left")]]'
-    latestSearch = '//a[i[contains(@class, "fa-angle-double-right")]]'
+    baseUrl = 'https://derideal.com/'
+    imageSearch = ('//img[contains(@class, "comic-page")]', '//img[contains(@class, "comic-pag")]')
+    prevSearch = '//a[contains(@data-bs-title, "Previous")]'
+    latestSearch = '//a[contains(@data-bs-title, "Latest")]'
     starter = indirectStarter
 
-    def __init__(self, name, sub, first, last=None):
+    def __init__(self, name, sub, first, last=None, multipleImagesPerStrip=False):
         if name == 'Derideal':
             super(Derideal, self).__init__(name)
         else:
@@ -22,13 +22,17 @@ class Derideal(_ParserScraper):
         self.stripUrl = self.url + '/%s/'
         self.firstStripUrl = self.stripUrl % first
         self.startUrl = self.firstStripUrl
+        self.multipleImagesPerStrip = multipleImagesPerStrip
+
+        self.pages = {}
+        self.lastChapter = ''
 
         if last:
             self.endOfLife = True
 
     def starter(self):
         indexPage = self.getPage(self.url)
-        self.chapters = indexPage.xpath('//a[contains(text(), "Read this episode")]/@href')
+        self.chapters = indexPage.xpath('//div[contains(@class, "main-episodes")]//article//a/@href')
         self.currentChapter = len(self.chapters)
         return indirectStarter(self)
 
@@ -39,18 +43,37 @@ class Derideal(_ParserScraper):
         filename = filename.replace('hogar-prision', 'home-prison')
         filename = filename.replace('strip', 'pe').replace('purpurina-effect', 'pe')
         filename = filename.replace('sector-de-seguridad', 'security-sector')
+        if self.multipleImagesPerStrip:
+            if pageUrl not in self.pages:
+                self.pages[pageUrl] = 1
+            else:
+                filename = 'img' + str(self.pages[pageUrl]) + '-' + filename
+                self.pages[pageUrl] += 1
+        if self.lastChapter != '' and pageUrl != self.lastChapter:
+            self.currentChapter -= 1
+            self.lastChapter = ''
         filename = 'ch' + str(self.currentChapter) + '-' + filename
         if pageUrl in self.chapters:
-            self.currentChapter -= 1
+            self.lastChapter = pageUrl
         return filename
+
+    def getPrevUrl(self, url, data):
+        # Fix missing navigation links between chapters
+        if 'nova/xen-aftermath' in url:
+            return self.stripUrl % 'xen-deadlock'
+        elif 'nova/xen-deadlock' in url:
+            return self.stripUrl % 'xen97-end'
+        return super(Derideal, self).getPrevUrl(url, data)
 
     @classmethod
     def getmodules(cls):
         return (
-            cls('Derideal', 'derideal', 'cover-prime'),
+            cls('Derideal', 'derideal', 'chimeras-cover'),
             cls('Legacy', 'derideal-legacy', 'the-dream-cover', last='derideal-is-on-hiatus'),
-            cls('LRE', 'RLE', 'the-leyend-of-the-rose-cover'),
+            cls('LostMemories', 'lost-memories', 'lost-memories-pixi', multipleImagesPerStrip=True),
+            cls('LRE', 'RLE', 'the-leyend-of-the-rose-cover', last='lre-47-ending'),
+            cls('Nova', 'nova', 'xen-prelude-cover', multipleImagesPerStrip=True),
             cls('ProjectPrime', 'project-prime', 'custus-part-i-cover'),
             cls('PurpurinaEffect', 'purpurina-effect', 'purpurina-effect-cover'),
-            cls('TheVoid', 'the-void', 'the-void-cover'),
+            cls('TheVoid', 'the-void', 'the-void-cover', last='arma-2-42-end'),
         )
